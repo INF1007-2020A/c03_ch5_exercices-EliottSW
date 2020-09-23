a9b4ecced4e5cde639e271af11af29f007481d88
Eliott Schuhmacher-Wantz <eliott.schuhmacher-wantz@polymtl.ca>
2020-09-23 10:11:28 -0400

    exo final

--- exercice.py
+++ exercice.py
@@ -6,43 +6,67 @@ from typing import List
 
 
 def convert_to_absolute() -> float:
-    nombre = int(input('Entrez un nombre'))
-    return abs(nombre)
+    mon_nombre = float(input('Veuillez entrer un nombre: '))
+    if mon_nombre < 0:
+        mon_nombre = -mon_nombre
+    return mon_nombre
 
 
 def use_prefixes() -> List[str]:
-    prefixes, suffixes = 'JKLMNOP', 'ack'
-    return [prefixe+suffixes for prefixe in prefixes]
+    prefixes, suffixe = 'JKLMNOP', 'ack'
+    resultat = []
+    for prefixe in prefixes:
+        nom = prefixe + suffixe
+        resultat.append(nom)
+    return resultat
+
+    #return [prefixe+suffixe for prefixe in prefixes]
 
 
 def prime_integer_summation() -> int:
-    return 0
+    nombrePremier = 0
+    nombre = 2
+    sommes = 0
+    while(nombrePremier < 100):
+        estPremier = True
+        for i in range(1, nombre+1):
+            if (nombre % i == 0 and i != 1 and i != nombre):
+                estPremier = False
+                break
+        if estPremier:
+            nombrePremier += 1
+            sommes += nombre
+        nombre += 1
+    return sommes
 
 
 def factorial(number: int) -> int:
-    return 0
+    result = 1
+    if number != 0:
+        for i in range(number):
+            result *= (number - i)
+    return result
 
 
 def use_continue() -> None:
-    for index in range(1 , 11):
-        if index == 5:
+    for chiffre in range(1, 11):
+        if chiffre == 5:
             continue
-        print(index)
-
+        print(chiffre)
 
 
 def main() -> None:
-    # print(f"La valeur absolue du nombre est {convert_to_absolute()}")
+    #print(f"La valeur absolue du nombre est {convert_to_absolute()}")
 
-    # print(f"La liste des noms générés avec les préfixes est: {use_prefixes()}")
+    #print(f"La liste des noms générés avec les préfixes est: {use_prefixes()}")
 
-    # print(f"La somme des nombres de 0 à 100 est: {prime_integer_summation()}")
+    #print(f"La somme des nombres de 0 à 100 est: {prime_integer_summation()}")
 
-    # number = 10
-    # print(f"La factiorelle du nombre {number} est: {factorial(number)}")
+    number = 10
+    print(f"La factiorelle du nombre {number} est: {factorial(number)}")
     
-    print(f"L'affichage de la boucle est:")
-    use_continue()
+    #print(f"L'affichage de la boucle est:")
+    #use_continue()
 
 
 if __name__ == '__main__':
